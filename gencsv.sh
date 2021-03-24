c5c7ac55e30f8aa39c35e6d7fe5d3824d2612fa8
rajan.shrivastav <rajaniilm@rediffmail.com>
2021-03-24 14:58:32 +0530

    Final code

--- gencsv.sh
+++ gencsv.sh
@@ -1,9 +1,26 @@
+# lower_limit=0
+# upper_limit=10
+# read -p "Enter a Number greater than 1: " counter
+# if [ -z $counter ];
+# then
+# counter=upper_limit
+# fi
+# # echo "$counter, `shuf -i 0-10 -n 1`"
+# ((counter--))
+# until ((counter < 0)); do
+# # echo "$counter, `shuf -i $lower_limit-$upper_limit -n 1`"
+# echo "$counter, `od -An -N2 -i /dev/random`"
+# ((counter--))
+# done > inputFile
+FILE=inputFile
 lower_limit=0
 upper_limit=10
-read -p "Enter a Number great than 1: " counter
-echo "$counter, `shuf -i 0-10 -n 1`"
-((counter--))
-until ((counter < 0)); do
-echo "$counter, `shuf -i $lower_limit-$upper_limit -n 1`"
-((counter--))
-done > inputFile
+read -p "Enter a Number greater than 1: " counter
+if [ -z $counter ];
+then
+counter=upper_limit
+fi
+for ((i=0;i<$counter;i++));
+do
+echo "$i, `od -An -N2 -i /dev/random`" >> $FILE
+done
